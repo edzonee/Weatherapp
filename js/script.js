dfacb272b40785badf4920a60c0947b7dfc77fca
edzonee <edzone1996@gmail.com>
2022-01-02 21:27:02 +0100

    8th commit
    added comments

--- js/script.js
+++ js/script.js
@@ -17,13 +17,16 @@ let divFiveDays = document.getElementById("fiveDays");
 userSubmit.addEventListener("click", function (e) {
   let city = userInput.value;
   let country = userCountry.value;
+  //api for current weather
   let urlCurrent = `https://api.weatherbit.io/v2.0/current?key=8a23c972397e47c09f3a3188e596ff7f&lang=sv&units=m&city=${city}&country=${country}`;
+  //api for forecast weather
   let urlForeCast = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&lang=sv&country=${country}&key=${API_KEY}`;
 
   e.preventDefault();
   city = userInput.value;
   userSubmit = userInput;
 
+  //displays error msg if input is null
   if (userInput.value === "") {
     clearSecondDiv();
     errorMsg.innerText = "Var vänlig och skriv en stad...";
@@ -32,11 +35,12 @@ userSubmit.addEventListener("click", function (e) {
   } else {
     clearError();
 
+    //Hovereffect on current weather
     $(divBorder).on("mouseenter", function () {
       $("#results").css("backgroundColor", "black");
       $("#results").css("color", "white");
     });
-
+    //remove hovereffect once mouse leaves
     $(divBorder).on("mouseleave", function () {
       $("#results").css("backgroundColor", "rgb(235, 235, 235)");
       $("#results").css("color", "black");
@@ -54,16 +58,22 @@ userSubmit.addEventListener("click", function (e) {
         divBorder.style.display = "block";
         divFiveDays.style.display = "flex";
 
-        cityTitle.innerText =
-          userSubmit.value + " " + data.data[0].country_code;
+        // Shows current weather 
+        // City and country
+        cityTitle.innerText = userSubmit.value + " " + data.data[0].country_code;
+        //Temperature
         cityTemp.innerText = data.data[0].temp + " " + "\xB0" + "C";
+        //Icon
         let icon = data.data[0].weather.icon;
         weatherIcon.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
+        // wind speed
         cityWind.innerText =
           "Vindhastighet" + " " + data.data[0].wind_spd + " " + "m/s";
-        cityDesc.innerText =
+        // description
+          cityDesc.innerText =
           "Beskrivning av vädret:" + " " + data.data[0].weather.description;
-        cityHumudity.innerText =
+        //humidity
+          cityHumudity.innerText =
           "Luftfuktighet: " + " " + data.data[0].rh + "%";
         divBorder.style.boxShadow = "0 2px 10px 0 rgba(93, 95, 96, 0.83)";
         divBorder.style.backgroundColor = "rgb(235, 235, 235)";
@@ -82,7 +92,9 @@ userSubmit.addEventListener("click", function (e) {
         }
       })
       .then(function (data) {
+        //Weather for 5 days forward
         for (let i = 1; i < 6; i++) {
+          //creates necessary elements
           const makeFiveDivs = document.createElement("div");
           const cityTitleDiv = document.createElement("h2");
           const weatherIconDiv = document.createElement("img");
@@ -90,11 +102,15 @@ userSubmit.addEventListener("click", function (e) {
           const cityDescDiv = document.createElement("p");
           const cityTempDiv = document.createElement("p");
 
+          //shows date on the next coming days.
           cityTitleDiv.innerText = data.data[i].valid_date;
           weatherIconDiv.src = `https://www.weatherbit.io/static/img/icons/${icon}.png`;
+          //description
           cityDescDiv.innerText = data.data[i].weather.description;
+          //temperature
           cityTempDiv.innerText = data.data[i].temp + " " + "\xB0" + "C";
 
+          //appends to DOM
           makeFiveDivs.appendChild(cityTitleDiv);
           makeFiveDivs.appendChild(weatherIconDiv);
           makeFiveDivs.appendChild(cityTempDiv);
@@ -114,6 +130,7 @@ userSubmit.addEventListener("click", function (e) {
   }
 });
 
+//clears current weather results
 function clearDiv() {
   const divElement = document.querySelectorAll("#results *");
   for (let element of divElement) {
@@ -122,6 +139,7 @@ function clearDiv() {
   }
 }
 
+//clears weather for five upcoming days
 function clearSecondDiv() {
   const divEl = document.querySelectorAll("#fiveDays *");
   for (let i = 0; i < divEl.length; i++) {
@@ -130,6 +148,7 @@ function clearSecondDiv() {
   }
 }
 
+//clears error messages
 function clearError() {
   const divElement = document.querySelectorAll("#errorDiv *");
   for (let element of divElement) {
@@ -137,6 +156,7 @@ function clearError() {
   }
 }
 
+//removes current weather box and a div with no content and only displays error message
 function clearWeather() {
   divFiveDays.style.display = "none";
   divBorder.style.display = "none";
